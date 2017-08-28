128505c9667af6ee18c3ddf3c8956123506273e4
rhosseini <hosseini.ec@gmail.com>
2017-08-27 22:24:52 -0400

    Adding PCEX Progress Calculation

---
+++ src/Content.java
@@ -0,0 +1,26 @@
+
+
+public class Content {
+	private String idName;
+	private String type;
+	
+	public Content(String idName, String type) {
+		super();
+		this.idName = idName;
+		this.type = type;
+	}
+	public String getIdName() {
+		return idName;
+	}
+	public void setIdName(String idName) {
+		this.idName = idName;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+}
